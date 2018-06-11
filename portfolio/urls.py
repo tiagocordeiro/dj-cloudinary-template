f6592a33b9cac6f1576c3287d68c8cf55081589f
Tiago Cordeiro <tiago@mulhergorila.com>
2018-06-10 22:34:38 -0300

    Adiciona Portfolio detalhe

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('portfolio/', views.portfolio, name='portfolio'),
+    path('portfolio/<int:pk>/', views.portfolio_detail, name='portfolio-detail'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,7 @@ from .models import Trabalho
 def portfolio(request):
     trabalhos = Trabalho.objects.all()
     return render(request, 'portfolio.html', {'trabalhos': trabalhos})
+
+def portfolio_detail(request, pk):
+    trabalho = Trabalho.objects.get(pk=pk)
+    return render(request, 'portfolio-detail.html', {'trabalho': trabalho})
